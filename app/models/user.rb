e1d00b9d1977e551b27f46b407fa7ddfb778c403
youri0416 <withuuuu0416@gmail.com>
2024-09-18 17:51:51 +0900

    バリデーション、アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :profile, :occupation, :position, presence: true
+
+  has_many :prototypes
+  has_many :comments
 end
