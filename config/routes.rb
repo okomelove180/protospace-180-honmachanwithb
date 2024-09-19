ba028ffabc69058e32b5bbf080ec0cf78d862e13
okomelove180 <toru.camp180@gmail.com>
2024-09-19 12:35:01 +0900

    prototypeコントローラ_ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
