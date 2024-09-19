d5af69d3be82c97e2879005420e9ae110790b0ed
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-19 12:08:21 +0900

    ユーザー詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
-  resources :prototypes, only:[:new, :create]
+  resources :prototypes
   resources :users, only:[:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
