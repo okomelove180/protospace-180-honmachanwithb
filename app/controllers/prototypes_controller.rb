a2c2d9399f0d3861be36f281ce566d63fb721d88
okomelove180 <toru.camp180@gmail.com>
2024-09-19 12:36:57 +0900

    edit/update/destroyコントローラ作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,26 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    redirect_to prototype_path(prototype)
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+end
+
+private
+
+def prototype_params
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
 end
