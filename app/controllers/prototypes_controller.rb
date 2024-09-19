0e4b0a1b7bfb12a431a9b81ccb50928ed245f957
okomelove180 <toru.camp180@gmail.com>
2024-09-19 15:04:09 +0900

    edit/update/destroyコントローラの作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,26 +1,41 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update, :destroy] # showアクションにも使いたい場合は、only: [:show, :edit, :update, :destroy]とする
+
   def index
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    redirect_to root_path if current_user.id != @prototype.user_id
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    redirect_to prototype_path(prototype)
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
+    if @prototype.user_id == current_user.id
+      if @prototype.destroy
+        redirect_to root_path, notice: '削除が完了しました'
+      else
+        redirect_to prototype_path(@prototype), alert: '削除に失敗しました'
+      end
+    else
+      redirect_to root_path, alert: '削除権限がありません'
+    end
   end
-end
 
-private
+  private
 
-def prototype_params
-  params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @prototype = Prototype.find(params[:id])
+  end
 end
